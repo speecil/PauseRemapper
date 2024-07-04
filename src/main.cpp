0e4266ae11f3e6ec411d8095d46bfc80ae8e76bf
Riley <90689870+speecil@users.noreply.github.com>
2024-07-04 13:34:26 +1000

    what is going on

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,28 @@
 #include "lapiz/shared/zenject/Zenjector.hpp"
 #include "Installers/PauseRemapperInstaller.hpp"
 
+#include "Logging.hpp"
+
 using namespace PauseRemapper::UI;
 using namespace GlobalNamespace;
 using namespace UnityEngine;
 
-static CModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
+static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 
-// Called at the early stages of game loading
-PAUSE_REMAPPER_EXPORT_FUNC void setup(CModInfo &info)
+PAUSE_REMAPPER_EXPORT_FUNC void setup(CModInfo* info)
 {
-    info.id = MOD_ID;
-    info.version = VERSION;
-    modInfo = info;
+    *info = modInfo.to_c();
+    INFO("Completed setup!");
 }
 
-// Called later on in the game loading - a good time to install function hooks
 PAUSE_REMAPPER_EXPORT_FUNC void late_load()
 {
     il2cpp_functions::Init();
     custom_types::Register::AutoRegister();
-    // Initialises BSML**** and a gameplay settings menu
+
     BSML::Init();
     BSML::Register::RegisterGameplaySetupTab("Pause Remapper", MOD_ID "_settings", UIManager::get_instance(), BSML::MenuType::All);
+
     auto zenjector = Lapiz::Zenject::Zenjector::Get();
     zenjector->Install<PauseRemapper::Installers::PauseRemapperInstaller*, GlobalNamespace::StandardGameplayInstaller*>();
 }
